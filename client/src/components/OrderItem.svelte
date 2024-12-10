ff450181f6a3ce1a68da982de6b1bd85f2a6ad9e
Fakurio <kamil.zelazowski.2@gmail.com>
2024-12-10 14:26:54 +0100

    Adding accordion on orders page

--- client/src/components/OrderItem.svelte
+++ client/src/components/OrderItem.svelte
@@ -4,6 +4,7 @@
   import { get } from "svelte/store";
   import { formatDate } from "../utils/formatDate";
   import {Skeleton} from "$lib/components/ui/skeleton";
+  import * as Accordion from "$lib/components/ui/accordion/index.js";
   import type {Manga} from "../types/manga";
 
   interface OrderItemProps {
@@ -35,36 +36,40 @@
 {#await prepareContent()}
   <Skeleton class="h-[100px] border border-card-foreground w-full"/>
 {:then _}
-    <details class="rounded-md p-4 cursor-pointer border-2 border-border">
-      <summary class="flex flex-wrap gap-10">
-        <div class="flex flex-col justify-evenly gap-4">
-          <p>
-            Status: <span class="text-primary font-bold">{status}</span>
-          </p>
-          <p>
-            Order value: <span class="text-primary font-bold">{total} PLN</span>
-          </p>
-          <p>
-            Order date: <span class="text-primary font-bold">{formatDate(date)}</span>
-          </p>
-        </div>
-        <div class="flex flex-wrap gap-5">
-          {#each orderItemsMangas as manga (manga.manga_id)}
-            <div>
-              <img src={manga.img_url} alt={manga.title} class="w-[100px] h-[150px]"/>
+    <Accordion.Item value={orderItems[0].order_id.toString()} class="rounded-md p-4 border-2 border-border">
+        <Accordion.Trigger class="p-0 hover:no-underline">
+            <div class="flex flex-wrap gap-10">
+                <div class="flex flex-col justify-evenly items-start gap-4">
+                    <p>
+                        Status: <span class="text-primary font-bold">{status}</span>
+                    </p>
+                    <p>
+                        Order value: <span class="text-primary font-bold">{total} PLN</span>
+                    </p>
+                    <p>
+                        Order date: <span class="text-primary font-bold">{formatDate(date)}</span>
+                    </p>
+                </div>
+                <div class="flex flex-wrap gap-5">
+                    {#each orderItemsMangas as manga (manga.manga_id)}
+                        <div>
+                            <img src={manga.img_url} alt={manga.title} class="w-[100px] h-[150px]"/>
+                        </div>
+                    {/each}
+                </div>
             </div>
-          {/each}
-        </div>
-      </summary>
-      <div class="mt-8 flex flex-col gap-5 text-[1.1rem]">
-        {#each orderItemsMangas as manga (manga.manga_id)}
-            <div class="border-border border-2 rounded-xl p-4 flex justify-between items-center max-[500px]:flex-col max-[500px]:gap-4">
-              <img src={manga.img_url} alt={manga.title} class="w-[100px] h-[150px]"/>
-              <p>Quantity: {manga.quantity}</p>
-              <p>Price: {manga.price} PLN</p>
+        </Accordion.Trigger>
+        <Accordion.Content>
+            <div class="mt-8 flex flex-col gap-5 text-[1.1rem]">
+                {#each orderItemsMangas as manga (manga.manga_id)}
+                    <div class="border-border border-2 rounded-xl p-4 flex justify-between items-center max-[500px]:flex-col max-[500px]:gap-4">
+                        <img src={manga.img_url} alt={manga.title} class="w-[100px] h-[150px]"/>
+                        <p>Quantity: {manga.quantity}</p>
+                        <p>Price: {manga.price} PLN</p>
+                    </div>
+                {/each}
             </div>
-        {/each}
-      </div>
-    </details>
+        </Accordion.Content>
+    </Accordion.Item>
 {/await}
 
--- client/src/components/OrderList.svelte
+++ client/src/components/OrderList.svelte
@@ -2,17 +2,18 @@
   import OrderItem from "./OrderItem.svelte";
   import { filteredOrderStore, fetchUserOrders } from "../stores/order.store";
   import {Skeleton} from "$lib/components/ui/skeleton";
+  import * as Accordion from "$lib/components/ui/accordion/index.js";
 </script>
 
 {#await fetchUserOrders()}
   <Skeleton class="h-[300px] border border-card-foreground w-full mt-4"/>
 {:then _}
-  <div class="mt-4 flex flex-col gap-10">
+  <Accordion.Root type="single" class="mt-4 flex flex-col gap-10">
     {#each $filteredOrderStore as order (order.order_id)}
       <OrderItem
               total={order.total_price}
               status={order.order_status} orderItems={order.orderItems} date={order.order_date}/>
     {/each}
-  </div>
+  </Accordion.Root>
 {/await}
 
